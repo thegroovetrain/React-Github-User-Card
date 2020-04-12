f9e783472bf2e43f2e095e4b4fd45599ef16072e
thegroovetrain <ericthegroovetrain@gmail.com>
2020-04-12 19:03:17 -0400

    app created

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      // state here
+    }
+  }
+
+  render() {
+    return (
+      <div>App Test</div>
+    )
+  }
+
+}
